932e3b161d55c665d4d3da84deba962f60ad1f0e
pri0807 <pri060@gmail.com>
2025-02-03 15:44:14 +0530

    new change
    new change

--- 01-docker-terraform/2_DOCKER_SQL/ingest_zone_data.py
+++
@@ -1,7 +0,0 @@
-import os
-import pandas as pd
-from sqlalchemy import create_engine
-
-df=pd.read_csv("taxi_zone_lookup.csv")
-engine=create_engine("postgresql://postgres:postgres@localhost:5433/ny_taxi")
-df.to_sql(name="zones",con=engine,if_exists="replace")
